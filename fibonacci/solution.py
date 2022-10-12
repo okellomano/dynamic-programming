350557e39831d8803f35b6d719f4c8686cb5d7a4
Mano <onyandookello@gmail.com>
2022-10-12 13:39:05 +0300

    using cache attribute to avoid using the global variable

--- fibonacci/solution.py
+++ fibonacci/solution.py
@@ -1,13 +1,13 @@
-fibonacci_cache = {}
 
 def fibonacci(n):
     if n <= 2:
         return 1
-    if n not in fibonacci_cache:
-        fibonacci_cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
-    return fibonacci_cache[n]
+    if not hasattr(fibonacci, 'cache'):
+        fibonacci.cache = {}
+    if n not in fibonacci.cache:
+        fibonacci.cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci.cache[n]
 
 
 if __name__ == '__main__':
     print(fibonacci(6))
-    
